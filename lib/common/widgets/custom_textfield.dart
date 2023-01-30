74d1b601a9c26eae1b5f13110a5de30b5e5a6ef7
AndikaTP <andikamufc@gmail.com>
2023-01-30 13:35:32 +0700

    create a common textfield and form

---
+++ lib/common/widgets/custom_textfield.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: TextFormField(
+        controller: controller,
+        obscureText: hintText == 'Password' ? true : false,
+        keyboardType: hintText == 'Email'
+            ? TextInputType.emailAddress
+            : TextInputType.text,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black38,
+            ),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black38,
+            ),
+          ),
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,3 +1,5 @@
+import 'package:amazon/common/widgets/custom_textfield.dart';
+import 'package:amazon/constant/color.dart';
 import 'package:flutter/material.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -8,9 +10,79 @@ class AuthScreen extends StatefulWidget {
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
+enum Auth {
+  signUp,
+  signIn,
+}
+
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signUp;
+  final GlobalKey _signUpFormKey = GlobalKey<FormState>();
+  final GlobalKey _signInFormKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Widget radioAuth(String title, Auth auth) {
+    return RadioListTile(
+      contentPadding: const EdgeInsets.only(left: 0),
+      value: auth,
+      groupValue: _auth,
+      onChanged: (Auth? auth) => setState(() => _auth = auth!),
+      title: Text(title),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _nameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: GlobalVariable.greyBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Welcome',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                ),
+                radioAuth('Create an Account', Auth.signUp),
+                if (_auth == Auth.signUp)
+                  Form(
+                    key: _signUpFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          controller: _nameController,
+                          hintText: 'Name',
+                        ),
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                      ],
+                    ),
+                  ),
+                radioAuth('Sign In', Auth.signIn),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
