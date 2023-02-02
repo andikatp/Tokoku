4d313f0cb34c2e4280e29766dfd863710cea25ef
AndikaTP <andikamufc@gmail.com>
2023-02-02 12:08:06 +0700

    install jsonwebtoken and make signin function on server

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require("../models/user");
 const authRoute = express.Router();
+const jwt = require('jsonwebtoken');
 
+
+//signup
 authRoute.post('/api/signup', async (req, res) => {
     try{
         const {name, email, password} = req.body;
@@ -22,6 +25,25 @@ authRoute.post('/api/signup', async (req, res) => {
         res.status(500).json({error: e.message});
     }
   
+});
+
+//signin
+authRoute.post('/api/signin', async (req, res)=>{
+try{
+    const {email, password} = req.body;
+    const existingUser = await User.findOne({email});
+    if(!existingUser){
+        return res.status(400).json({msg: "User with this email does not exist"});
+    }
+    const isMatch = await bcryptjs.compare(password, existingUser.password);
+    if(!isMatch){
+        return res.status(400).json({msg: "Incorrect Password"});
+    }
+    const token = jwt.sign({id: existingUser.id}, "passwordKey" );
+    res.json({token, ...existingUser._doc});
+}catch(e){
+    res.status(500).json({error: e.message});
+}
 });
 
 module.exports = authRoute;
