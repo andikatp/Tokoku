60ebbf6e70503a1ef1bdfdf0ec32e4afcb21d09c
AndikaTP <andikamufc@gmail.com>
2023-02-09 00:04:13 +0700

    implement the pick images function on dotted box

--- lib/constant/utils.dart
+++ lib/constant/utils.dart
@@ -1,5 +1,28 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackbar(ScaffoldMessengerState messenger, String text) {
   messenger.showSnackBar(SnackBar(content: Text(text)));
+}
+
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+
+  try {
+    FilePickerResult? file = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: true,
+    );
+    if (file != null && file.files.isNotEmpty) {
+      for (int i = 0; i < file.files.length; i++) {
+        images.add(File(file.files[i].path!));
+      }
+    }
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+  return images;
 }
--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:amazon/common/widgets/custom_elevated_button.dart';
 import 'package:amazon/common/widgets/custom_textfield.dart';
 import 'package:amazon/constant/global_variable.dart';
+import 'package:amazon/constant/utils.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +20,8 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController _price = TextEditingController();
   final TextEditingController _quantity = TextEditingController();
   String _category = 'Mobiles';
+  List<File> _images = [];
+
   final List<String> _productCategories = [
     'Mobiles',
     'Essentials',
@@ -32,6 +37,11 @@ class _AddProductScreenState extends State<AddProductScreen> {
     _price.dispose();
     _quantity.dispose();
     super.dispose();
+  }
+
+  void _pickPhoto() async {
+    List<File> res = await pickImages();
+    _images = res;
   }
 
   @override
@@ -56,30 +66,33 @@ class _AddProductScreenState extends State<AddProductScreen> {
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 30),
-                child: DottedBorder(
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(15),
-                  dashPattern: const [10, 4],
-                  padding: const EdgeInsets.all(10),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(12)),
-                    child: SizedBox(
-                      height: 200,
-                      width: double.infinity,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.folder_open,
-                            size: 40,
-                          ),
-                          const SizedBox(height: 20),
-                          Text(
-                            'Select Product Image',
-                            style: TextStyle(
-                                fontSize: 15, color: Colors.grey[400]),
-                          ),
-                        ],
+                child: GestureDetector(
+                  onTap: _pickPhoto,
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(15),
+                    dashPattern: const [10, 4],
+                    padding: const EdgeInsets.all(10),
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(12)),
+                      child: SizedBox(
+                        height: 200,
+                        width: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.folder_open,
+                              size: 40,
+                            ),
+                            const SizedBox(height: 20),
+                            Text(
+                              'Select Product Image',
+                              style: TextStyle(
+                                  fontSize: 15, color: Colors.grey[400]),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
