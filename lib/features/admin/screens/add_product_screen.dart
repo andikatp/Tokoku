93cfb51bfbd48626f053ed4bd71bf70ddde8c01f
AndikaTP <andikamufc@gmail.com>
2023-02-10 11:01:39 +0700

    created a carousel on pick image

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -4,6 +4,7 @@ import 'package:amazon/common/widgets/custom_elevated_button.dart';
 import 'package:amazon/common/widgets/custom_textfield.dart';
 import 'package:amazon/constant/global_variable.dart';
 import 'package:amazon/constant/utils.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -41,7 +42,7 @@ class _AddProductScreenState extends State<AddProductScreen> {
 
   void _pickPhoto() async {
     List<File> res = await pickImages();
-    _images = res;
+    setState(() => _images = res);
   }
 
   @override
@@ -68,34 +69,51 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 padding: const EdgeInsets.only(bottom: 30),
                 child: GestureDetector(
                   onTap: _pickPhoto,
-                  child: DottedBorder(
-                    borderType: BorderType.RRect,
-                    radius: const Radius.circular(15),
-                    dashPattern: const [10, 4],
-                    padding: const EdgeInsets.all(10),
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(12)),
-                      child: SizedBox(
-                        height: 200,
-                        width: double.infinity,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Icon(
-                              Icons.folder_open,
-                              size: 40,
+                  child: _images.isNotEmpty
+                      ? CarouselSlider.builder(
+                          itemCount: _images.length,
+                          options: CarouselOptions(
+                            height: 200,
+                            autoPlay: true,
+                            viewportFraction: 1,
+                            enlargeCenterPage: false,
+                          ),
+                          itemBuilder: (context, index, realIndex) =>
+                              Image.file(
+                            _images[index],
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : DottedBorder(
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(15),
+                          dashPattern: const [10, 4],
+                          padding: const EdgeInsets.all(10),
+                          child: ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(12)),
+                            child: SizedBox(
+                              height: 200,
+                              width: double.infinity,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.folder_open,
+                                    size: 40,
+                                  ),
+                                  const SizedBox(height: 20),
+                                  Text(
+                                    'Select Product Image',
+                                    style: TextStyle(
+                                        fontSize: 15, color: Colors.grey[400]),
+                                  ),
+                                ],
+                              ),
                             ),
-                            const SizedBox(height: 20),
-                            Text(
-                              'Select Product Image',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.grey[400]),
-                            ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
                 ),
               ),
               CustomTextField(
